26a256a277a66680c72a89dae69ec2952644525f
Bowser <lisalreynolds2015@gmail.com>
2024-07-29 15:38:47 -0600

    updates to file structure

---
+++ controllers/thought-controllers.js
@@ -0,0 +1,15 @@
+//write our functions for thoughts
+
+//get all thoughts
+
+//get a single thought by unique id
+
+//create a new thought
+
+//update a thought by unique id
+
+// delete a thought by unique id
+
+//create a reaction and store in thoughts array
+
+//delete a reaction by unique id
